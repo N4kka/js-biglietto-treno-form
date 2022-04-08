375f57983a83a2f0a41adde52928f727c0659b45
N4kka <sarlinicola7@gmail.com>
2022-04-08 17:42:22 +0200

    ticket calculator added with random number and different prices

--- js/script.js
+++ js/script.js
@@ -3,16 +3,17 @@
 //Declaration
 //Name
 const formBtn = document.getElementById("generate");
-const formSelect = document.getElementById("age");
+const kmInput = document.getElementById("km");
+const ageSelect = document.getElementById("age");
 
 //Elaboration
 generate.addEventListener("click",
     function() {
         //Price calculation with km and age
-        const userKm = parseInt(document.getElementById("km"));
-        console.log("km");
-        const userAge = parseInt(document.getElementById("age"));
-        console.log("age");
+        const userKm = parseInt(kmInput.value);
+        console.log(userKm);
+        const userAge = ageSelect.value;
+        console.log(userAge);
         //Price calculation
         let price = 0.21;
 
@@ -20,15 +21,22 @@ generate.addEventListener("click",
 
         cost = price * userKm;
 
-        if ('Minorenne') {
+        if (userAge === 'Minorenne') {
             cost = cost - (cost * 20 / 100);
-        }   else if ('Maggiorenne' >=65) {
+        }   else if (userAge === 'Over 65') {
             cost = cost - (cost * 40 / 100);
         }   else {
             cost = price * userKm;
         }
+        
+        const random = (90000,92911);
+        Math.floor(Math.random() * (92911 - 90000) + 90000);
+        
         //Final Output
         //Showing the price for each age
-        document.getElementById('price').innerHTML = cost;
+        document.querySelector('.carrozza').innerHTML =
+        Math.floor(Math.random() * 11);
+        document.querySelector('.cp').innerHTML = random;
+        document.querySelector('.costo').innerHTML = cost;
     }
 );
