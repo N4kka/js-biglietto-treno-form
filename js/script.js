8009fec0fa71b8e86044f354113bf38c9bed49b8
N4kka <sarlinicola7@gmail.com>
2022-04-08 15:33:38 +0200

    problemi con il collegare le opzioni all'algoritmo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 //Train ticket calculation
 
 //Declaration
-document.getElementById("testFunction").innerHTML = "sono un test"; 
+//Name
+const formBtn = document.getElementById("generate");
+const formSelect = document.getElementById("age");
+
+//Elaboration
+generate.addEventListener("click",
+    function() {
+        //Price calculation with km and age
+        const userKm = parseInt(document.getElementById("km"));
+        console.log("km");
+        const userAge = parseInt(document.getElementById("age"));
+        console.log("age");
+        //Price calculation
+        let price = 0.21;
+
+        let cost;
+
+        cost = price * userKm;
+
+        if ('Minorenne') {
+            cost = cost - (cost * 20 / 100);
+        }   else if ('Maggiorenne' >=65) {
+            cost = cost - (cost * 40 / 100);
+        }   else {
+            cost = price * userKm;
+        }
+        //Final Output
+        //Showing the price for each age
+        document.getElementById('price').innerHTML = cost;
+    }
+);
